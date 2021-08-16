4db737ae7857f63302638f0803e867993869896a
yusei1125 <yusei11250925@gmail.com>
2021-08-16 20:28:51 +0000

    finish

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,59 @@
+class BooksController < ApplicationController
+  def top
+  end
+  
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    
+  end
+  
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+     flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book)
+
+    else
+      @books = Book.all
+      render("books/index")
+    end
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = 'Book was successfully updated.' 
+    redirect_to book_path(@book)
+    else
+    render("books/edit")
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy
+   book = Book.find(params[:id])
+   if book.destroy
+     flash[:notice] = 'Book was successfully destroyed.'
+    redirect_to books_path  
+   else 
+     render("books/index")
+   end
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
